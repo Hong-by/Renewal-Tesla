59b6ba0fba7851ee57701d3a15da25c3e1ba47f6
HongBY <hby033@naver.com>
2021-06-30 18:42:17 +0900

    6/30 2번째 페이지 3번 섹터까지 완료 / 조금만하면 끝날것 같다 / wow 플러그인 적용 완료. 페이지 스크롤 때마다 반복을 어떻게 넣어야 할지 고민 필요

---
+++ js/slide.js
@@ -0,0 +1,96 @@
+// 슬라이드 이미지 변수 저장
+const slide = document.querySelectorAll('.slideBox');
+
+// 각 버튼 및 변경 항목 저장 저장
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+const dots = document.querySelectorAll('.dot');
+const contxt = document.querySelectorAll('.slideTxt_body');
+
+// 슬라이드 초기 값 설정
+let click = 0;
+
+//슬라이드 출력
+function slideimg() {
+  slide[click].style.display = 'block';
+  dots[click].classList.add('active');
+  contxt[click].style.display = 'block';
+};
+slideimg();
+
+//이미지 초기화
+const clearSlide = () => {
+  for(let i = 0; i < slide.length; i++ ){
+    slide[i].style.display = 'none';
+    dots[i].classList.remove('active');
+    contxt[i].style.display = 'none';
+  };
+};
+
+//이미지 슬라이드 기능
+function slideShow(){
+  clearSlide();
+  
+  click += 1;
+
+  if(click > slide.length-1){
+    click = 0;
+  } else if (click < 0){
+    click=slide.length-1;
+  };
+
+  slideimg();
+  return click;
+};
+
+//이전 버튼 클릭시
+prev.addEventListener('click', function(){
+  clearSlide();
+  click -= 1;
+  if (click < 0){
+    click=slide.length-1;
+  }
+  // move = click;
+  slideimg();
+  return click;
+
+});
+//다음 버튼 클릭시
+next.addEventListener('click', function(){
+  clearSlide();
+  click += 1;
+  if(click > slide.length-1){
+    click = 0;
+  }
+  // move = click;
+  slideimg();
+  return click;
+
+});
+
+
+//각각의 닷 버튼 지정
+for(let n = 0; n < dots.length; n++){
+  dots[n].addEventListener('click', function(){
+    activeDot(n);
+  });
+}
+
+//닷 버튼 클릭시 슬라이드 이동
+function activeDot(n){
+  clearSlide();
+  click = n;
+  slideimg();
+  return click;
+};
+
+//슬라이드쇼 자동 실행
+let start = setInterval(slideShow, 3000);
+
+document.querySelector('.slideBox-wrap').addEventListener("mouseover", ()=>{
+  clearInterval(start);
+});
+
+document.querySelector('.slideBox-wrap').addEventListener("mouseout", ()=>{
+  start = setInterval(slideShow, 2000);
+});
